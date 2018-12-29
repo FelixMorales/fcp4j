1f6d348c0821ac15b33e546942b2945a5f8f7963
FelixMorales <felix_morales_@hotmail.com>
2018-12-29 15:37:34 -0400

    first commit

---
+++ src/main/java/com/ucab/fcp4j/App.java
@@ -0,0 +1,13 @@
+package com.ucab.fcp4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
